cb7cbe2f360e7e656ea8fedaa2fa580355be4a30
luanascardua <92794401+luanascardua@users.noreply.github.com>
2022-08-06 12:47:16 -0300

    style

--- sqlite.py
+++ sqlite.py
@@ -1,15 +1,14 @@
 from sqlite3 import Error
-from cores import Cores
 from tqdm import tqdm
-from sheet import *
-import sqlite3
-import time
 import os
+
+from cores import Cores
+from sheet import Sheet
 
 
 executeWb = Sheet()
-
-clean = lambda: os.system('cls'); clean()
+clean = lambda: os.system('cls')
+clean()
 
 
 def createDb(database):
@@ -21,21 +20,14 @@ def createDb(database):
     except sqlite3.Error as e:
         print(e)
 
-
 def connect():
-
     global cursor
     cursor = connection.cursor()
 
-
 def commit():
-    
     connection.commit()
 
-
 def table():
-
-
     #criar tabela tipoUsuario
     try:
         cursor.execute("""
@@ -44,7 +36,6 @@ def table():
                 descricao TEXT NOT NULL
                 );
         """)
-
         #criar tabela usuario
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS usuario(
@@ -55,7 +46,6 @@ def table():
                 FOREIGN KEY (siglaUsuario) REFERENCES tipoUsuario (siglaUsuario)
                 );
         """)
-
         #criar tabela tipoAvaliacao
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS tipoAvaliacao(
@@ -63,7 +53,6 @@ def table():
                 descricao TEXT NOT NULL
                 );
         """)
-
         #criar tabela avaliacao
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS avaliacao(
@@ -79,7 +68,6 @@ def table():
                 FOREIGN KEY (siglaAvaliacao) REFERENCES tipoAvaliacao (siglaAvaliacao)
                 );
         """)
-
         #criar tabela avaliacaoAluno
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS avaliacaoAluno(
@@ -94,16 +82,12 @@ def table():
 
     except sqlite3.Error as e:
         print(e)
-
     
 def insertTipoUsuario():
-    
     tipoUsuario = [
         ('PROF', 'Professor'),
         ('ESTD', 'Estudante')
     ]
-    
-
     try:
         cursor.executemany(
             "INSERT INTO tipoUsuario VALUES (?,?);", tipoUsuario
@@ -114,18 +98,15 @@ def insertTipoUsuario():
 
     commit()
 
-
 def insertUsuario():
-
     executeWb.countLines(sheets[0])
-    
+
     for lines in tqdm(range(executeWb.firstLine, executeWb.lastLine)):
         executeWb.getDataUser(lines)
         try:
             cursor.execute(
                 f"INSERT INTO usuario VALUES(null, '{executeWb.name}', '{executeWb.sigla}', '{executeWb.idade}')"
             )
-
             commit()
         except Exception as e:
             print(e)
@@ -134,14 +115,11 @@ def insertUsuario():
         
     print(f'{Cores.OKGREEN}\nDados inseridos com sucesso.')
     
-
 def insertTipoAvaliacao():
-
     executeWb.countLines(sheets[1])
 
     for lines in tqdm(range(executeWb.firstLine, executeWb.lastLine)):
         executeWb.getDataTipoAvaliacao(lines)
-        
         try:
             cursor.execute(
                 f"INSERT INTO tipoAvaliacao VALUES('{executeWb.sigla}', '{executeWb.descricao}')"
@@ -154,14 +132,11 @@ def insertTipoAvaliacao():
 
     print(f'{Cores.OKGREEN}\nDados inseridos com sucesso.')
 
-
 def insertAvaliacao():
-
     executeWb.countLines(sheets[2])
 
     for lines in tqdm(range(executeWb.firstLine, executeWb.lastLine)):
         executeWb.getDataAvaliacao(lines)
-
         try:
             cursor.execute(
                 f"INSERT INTO avaliacao VALUES (null, '{executeWb.sigla}', '{executeWb.descricao}', \
@@ -176,14 +151,11 @@ def insertAvaliacao():
 
     print(f'{Cores.OKGREEN}\nDados inseridos com sucesso.')
 
-
 def insertAvaliacaoAluno():
-
     executeWb.countLines(sheets[3])
 
     for lines in tqdm(range(executeWb.firstLine, executeWb.lastLine)):
         executeWb.getDataAvaliacaoAluno(lines)
-        
         try:
             cursor.execute(
                 f"INSERT INTO avaliacaoAluno VALUES('{executeWb.idUser}', '{executeWb.idAvaliacao}', \
@@ -197,11 +169,9 @@ def insertAvaliacaoAluno():
 
     print(f'{Cores.OKGREEN}\nDados inseridos com sucesso.')
 
-
 def update(tabela):
    
     print(f'{Cores.BOLD}{Cores.OKBLUE}\n*** Preencha as informações ***\n{Cores.ENDC}')
-    
     campo  = input('CAMPO: ')
     update = input('ALTERAR PARA: ')
     id     = input('INDIQUE O ID A ATUALIZAR: ')
@@ -216,14 +186,11 @@ def update(tabela):
     except Exception as e:
         print(e)
 
-
 def delete(tabela):
-
     print(f'{Cores.BOLD}{Cores.OKBLUE}\n*** Preencha as informações ***\n{Cores.ENDC}')
-
     campo = input('CAMPO DE REFERÊNCIA: ')
     value = input('VALUE: ')
-    
+
     try:
         cursor.execute(
             f"DELETE FROM {tabela.lower()} WHERE {campo} = '{value}'"        )
@@ -232,10 +199,8 @@ def delete(tabela):
     except Exception as e:
         print(e)
 
-
 #listar elementos das tabelas
 def displayTipoUsuario():
-    
     try:
         cursor.execute(
             "SELECT * FROM tipoUsuario"
@@ -253,9 +218,7 @@ def displayTipoUsuario():
     except Exception as e:
         print(e)
 
-
 def displayUsuario():
-
     try:
         cursor.execute(
             "SELECT * FROM usuario"
@@ -274,10 +237,8 @@ def displayUsuario():
             print(f'{Cores.BOLD}{Cores.FAIL}Não foram encontrados registros.{Cores.ENDC}')
     except Exception as e:
         print(e)
-    
 
 def displayTipoAvaliacao():
-
     try:
         cursor.execute(
             "SELECT * FROM tipoAvaliacao"
@@ -292,13 +253,10 @@ def displayTipoAvaliacao():
                 print("{:<10} {:<20}".format(resultado[item][0], resultado[item][1]))
         else:
             print(f"{Cores.BOLD}{Cores.FAIL}Não foram encontrados registros.{Cores.ENDC}")
-
     except Exception as e:
         print(e)
 
-
 def displayAvaliacao():
-
     try:
         cursor.execute("""
             SELECT idAvaliacao, siglaAvaliacao, descricao, tipoAvaliacao, date(dataInicio), date(dataTermino), notaAprovacao, idUser 
@@ -322,9 +280,7 @@ def displayAvaliacao():
     except Exception as e:
         print(e)
 
-
 def displayAvaliacaoAluno():
-
     try:
         cursor.execute("""
             SELECT idUser, idAvaliacao, date(dataConclusao), notaAtribuida
@@ -343,13 +299,10 @@ def displayAvaliacaoAluno():
                 resultado[item][2], resultado[item][3]))
         else:
             print(f"{Cores.BOLD}{Cores.FAIL}Não foram encontrados registros.{Cores.ENDC}")
-
     except Exception as e:
         print(e)
 
-
 def queryAvaliacaoPeriodo():
-
     #Por periodo
     print(f'{Cores.BOLD}{Cores.OKBLUE}\n*** Preencha as informações ***\n{Cores.ENDC}')
     periodoInicio   = input('Data início: ')
@@ -362,7 +315,6 @@ def queryAvaliacaoPeriodo():
         """)
 
         resultado = cursor.fetchall()
-      
         if resultado:
             print(f"{Cores.BOLD}{Cores.OKGREEN}")
             print("{:<10} {:<10} {:<30} {:15} {:<15} {:<10} {:<10}".format("ID", "SIGLA", "DESCRIÇÃO", "DATA Início",
@@ -374,13 +326,10 @@ def queryAvaliacaoPeriodo():
                 resultado[item][2], resultado[item][3], resultado[item][4], resultado[item][5], resultado[item][6],))
         else:
             print(f"{Cores.BOLD}{Cores.FAIL}Não foram encontrados registros.{Cores.ENDC}")
-        
     except Exception as e:
         print(e)
 
-
 def queryAvaliacaoProfessor():
-
     #Por professor
     professor = input('\nProfessor: ')
    
@@ -392,7 +341,6 @@ def queryAvaliacaoProfessor():
         """)
 
         resultado = cursor.fetchall()
-
         if resultado:
             print(f"{Cores.BOLD}{Cores.OKGREEN}")
             print("{:<10} {:<10} {:<20} {:<15} {:<15} {:<20}".format("ID", "SIGLA", "DESCRIÇÃO", "DATA Início",
@@ -405,11 +353,9 @@ def queryAvaliacaoProfessor():
         else:
             print(f"{Cores.BOLD}{Cores.FAIL}Não foram encontrados registros.{Cores.ENDC}")
     except Exception as e:
-        print(e)
-    
+        print(e)   
 
 def queryAvaliacaoTipo():
-
     tipo = input('\nTipo: ')
 
     try:
@@ -434,9 +380,7 @@ def queryAvaliacaoTipo():
     except Exception as e:
         print(e)
 
-
 def queryAlunosSemAvaliacao():
-
     #alunos sem nota atribuída
     try:
         cursor.execute("""
@@ -461,9 +405,7 @@ def queryAlunosSemAvaliacao():
     except Exception as e:
         print(e)
 
-
 def queryAlunosReprovados():
-
     #lista alunos reprovados
     try:
         cursor.execute("""
@@ -476,7 +418,6 @@ def queryAlunosReprovados():
         """)
 
         resultado = cursor.fetchall()
-
         if resultado:
             print(f"{Cores.BOLD}{Cores.OKGREEN}")
             print("{:<20} {:<10} {:<10}".format("ESTUDANTE", "SIGLA", "NOTA"))
@@ -490,9 +431,7 @@ def queryAlunosReprovados():
     except Exception as e:
         print(e)
 
-
 def queryAlunosAprovados():
-
     #lista alunos aprovados
     try:
         cursor.execute("""
@@ -505,7 +444,6 @@ def queryAlunosAprovados():
         """)
 
         resultado = cursor.fetchall()
-
         if resultado:
             print(f"{Cores.BOLD}{Cores.OKGREEN}")
             print("{:<20} {:<10} {:<10}".format("ESTUDANTE", "SIGLA", "NOTA"))
@@ -515,13 +453,10 @@ def queryAlunosAprovados():
                 print("{:<20} {:<10} {:<10}".format(resultado[item][0], resultado[item][1], resultado[item][2]))
         else:
             print(f"{Cores.BOLD}{Cores.FAIL}Não foram encontrados registros.{Cores.ENDC}")
-
     except Exception as e:
         print(e)
 
-
 def queryPesquisarNotas():
-
     #pesquisa e lista avaliações entre notas 
     print(f'{Cores.BOLD}{Cores.OKBLUE}\n*** Preencha as informações ***\n{Cores.ENDC}')
     i = input('Nota inicial: ')
@@ -538,7 +473,6 @@ def queryPesquisarNotas():
         """)
 
         resultado = cursor.fetchall()
-
         if resultado:
             print(f"{Cores.BOLD}{Cores.OKGREEN}")
             print("{:<20} {:<10} {:<10}".format("ESTUDANTE", "SIGLA", "NOTA"))
@@ -552,12 +486,9 @@ def queryPesquisarNotas():
     except Exception as e:
         print(e)
 
-
 def search():
-
     #Ppesquisa algum registro de acordo com o dado do campo informado
     print(f'{Cores.BOLD}{Cores.OKBLUE}\n*** Preencha as informações ***\n{Cores.ENDC}')
-    
     tabela   = input('TABELA: ')
     campo    = input('CAMPO: ')
     registro = input('REGISTRO: ')
@@ -574,13 +505,10 @@ def search():
                 print(resultado[item])
         else:
             print(f"{Cores.BOLD}{Cores.FAIL}Não foram encontrados registros.{Cores.ENDC}")
-
     except Exception as e:
         print(e)
 
-
 def boletim():
-
     #lista todas as avaliações e notas correspondentes ao nome do estudante informado 
     estudante = input('\nInforme seu nome: ')
 
@@ -594,7 +522,6 @@ def boletim():
             WHERE U.siglaUsuario = 'ESTD' and U.nome LIKE '%{estudante}%';
         """)
         resultado = cursor.fetchall()
-
         if resultado:
             print(f"{Cores.BOLD}{Cores.OKGREEN}")
             print("{:<10} {:<10}".format("SIGLA", "NOTA"))
@@ -604,10 +531,8 @@ def boletim():
                 print("{:<10} {:<10}".format(resultado[item][0], resultado[item][1]))
         else:
             print(f"{Cores.BOLD}{Cores.FAIL}Não foram encontrados registros.{Cores.ENDC}")
-
     except Exception as e:
         print(e)
-
 
 
 global functions
